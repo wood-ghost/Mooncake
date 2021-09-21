243dae79668d5f359614fb0a19e302600baeac58
wood-ghost <yyz2020@sjtu.edu.cn>
2021-09-21 21:39:22 +0800

    add picture.py and sentence.py

---
+++ picture.py
@@ -0,0 +1,19 @@
+print("Here is a big mooncake\n")
+
+
+                     
+print("\n")                     
+print("      ___   ___      \n")
+print("     / _ \ / _ \     \n")
+print("    | (_) | (_) |    \n")
+print("   ______/ \___/___  \n")
+print("  / _ \        / _ \ \n")
+print(" | (_) |      | (_) |\n")
+print("  \______   ___\___/ \n")
+print("     / _ \ / _ \     \n")
+print("    | (_) | (_) |    \n")
+print("     \___/ \___/     \n")
+print("                     \n")
+print("                     \n")
+
+
